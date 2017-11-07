731c412d14dee8ef1e002c1f5d3b01a87f60544a
bvarghese78 <benjivarghese@gmail.com>
2017-11-07 23:51:46 +0900

    fixed issue with index.js not working

---
+++ www/js/index.js
@@ -0,0 +1,9 @@
+(function(){
+    var f = function(){
+        
+        console.log('sending a message to the debugger');
+        document.body.style.background= "#f4a460";
+    }
+    
+    f();
+}());
